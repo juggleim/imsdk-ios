dd141899143172288ecfabf9fc215cffd08c72f1
Fei Li <14392240@qq.com>
2024-01-04 18:09:22 +0800

    conversation description

--- JetIM/JetIM/model/JConversationInfo.h
+++ JetIM/JetIM/model/JConversationInfo.h
@@ -13,14 +13,21 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface JConversationInfo : NSObject
-
+/// 会话标识
 @property (nonatomic, strong) JConversation *conversation;
+/// 当前会话的消息未读数
 @property (nonatomic, assign) int unreadCount;
+/// 会话更新时间（服务端时间）
 @property (nonatomic, assign) long long updateTime;
+/// 会话中的最后一条消息
 @property (nonatomic, strong) JMessage *lastMessage;
+/// 是否置顶
 @property (nonatomic, assign) BOOL isTop;
+/// 会话置顶时间
 @property (nonatomic, assign) long long topTime;
+/// 是否免打扰
 @property (nonatomic, assign) BOOL mute;
+/// 会话草稿
 @property (nonatomic, copy) NSString *draft;
 //TODO: mention
 //@property (nonatomic, strong) JMessage *lastMentionMessage;
