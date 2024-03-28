f7ad35cdb80e3a36dcac36c7cda637ae4b8f474a
Fei Li <14392240@qq.com>
2024-03-28 19:01:40 +0800

    fix crash

--- JetIM/JetIM/internal/JConversationManager.m
+++ JetIM/JetIM/internal/JConversationManager.m
@@ -172,21 +172,25 @@
 - (void)setDraft:(NSString *)draft inConversation:(JConversation *)conversation {
     [self.core.dbManager setDraft:draft inConversation:conversation];
     JConversationInfo *info = [self.core.dbManager getConversationInfo:conversation];
-    dispatch_async(self.core.delegateQueue, ^{
-        if ([self.delegate respondsToSelector:@selector(conversationInfoDidUpdate:)]) {
-            [self.delegate conversationInfoDidUpdate:@[info]];
-        }
-    });
+    if (info) {
+        dispatch_async(self.core.delegateQueue, ^{
+            if ([self.delegate respondsToSelector:@selector(conversationInfoDidUpdate:)]) {
+                [self.delegate conversationInfoDidUpdate:@[info]];
+            }
+        });
+    }
 }
 
 - (void)clearDraftInConversation:(JConversation *)conversation {
     [self.core.dbManager clearDraftInConversation:conversation];
     JConversationInfo *info = [self.core.dbManager getConversationInfo:conversation];
-    dispatch_async(self.core.delegateQueue, ^{
-        if ([self.delegate respondsToSelector:@selector(conversationInfoDidUpdate:)]) {
-            [self.delegate conversationInfoDidUpdate:@[info]];
-        }
-    });
+    if (info) {
+        dispatch_async(self.core.delegateQueue, ^{
+            if ([self.delegate respondsToSelector:@selector(conversationInfoDidUpdate:)]) {
+                [self.delegate conversationInfoDidUpdate:@[info]];
+            }
+        });
+    }
 }
 
 - (void)setMute:(BOOL)isMute
