ca5772ea2882dc0838ae0d7e9fa51f4155877e85
Fei Li <14392240@qq.com>
2024-07-31 20:26:37 +0800

    remove useless code

--- QuickStart/QuickStart/ViewController.swift
+++ QuickStart/QuickStart/ViewController.swift
@@ -15,10 +15,6 @@ let token4 = "CgZhcHBrZXkaIDHZwzfny4j4GiJye8y8ehU5fpJ+wVOGI3dCsBMfyLQv"
 let token5 = "CgZhcHBrZXkaIOx2upLCsmsefp8U/KNb52UGnAEu/xf+im3QaUd0HTC2"
 
 enum ButtonType: Int {
-    case user1
-    case user2
-    case user3
-    case user4
     case signIn
 }
 
@@ -31,28 +27,12 @@ class ViewController: UIViewController {
     var verifyCodeTextField: UITextField { connectView.verifyCodeTextField }
     var signInButton: UIButton { connectView.signInButton }
     
-    @IBOutlet weak var mainView: MainView!
-    
-    var homeStackView: UIStackView { mainView.homeStackView }
-    //    var startChatWithViewControllerButton: UIButton { mainView.groupChannelItemView.actionButton }
-    var user1Button: UIButton { mainView.user1View.actionButton }
-    var user2Button: UIButton { mainView.user2View.actionButton }
-    var user3Button: UIButton { mainView.user3View.actionButton }
-    var user4Button: UIButton { mainView.user4View.actionButton }
-    
     @IBOutlet weak var versionLabel: UILabel!
     
     @IBOutlet weak var loadingIndicator: UIActivityIndicatorView! {
         didSet {
             loadingIndicator.stopAnimating()
         }
-    }
-    
-    let duration: TimeInterval = 0.4
-    
-    enum CornerRadius: CGFloat {
-        case small = 4.0
-        case large = 8.0
     }
     
     
@@ -67,27 +47,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        user1Button.tag = ButtonType.user1.rawValue
-        user2Button.tag = ButtonType.user2.rawValue
-        user3Button.tag = ButtonType.user3.rawValue
-        user4Button.tag = ButtonType.user4.rawValue
         signInButton.tag = ButtonType.signIn.rawValue
-        
-        user1Button.addTarget(self, action: #selector(onTapButton(_:)), for: .touchUpInside)
-        user2Button.addTarget(self, action: #selector(onTapButton(_:)), for: .touchUpInside)
-        user3Button.addTarget(self, action: #selector(onTapButton(_:)), for: .touchUpInside)
-        user4Button.addTarget(self, action: #selector(onTapButton(_:)), for: .touchUpInside)
         signInButton.addTarget(self, action: #selector(onTapButton(_:)), for: .touchUpInside)
         
         UserDefaults.saveIsLightTheme(true)
         
-        self.mainView.isHidden = true
-        self.mainView.alpha = 0
         self.connectView.isHidden = false
         self.connectView.alpha = 1
-        
-        self.homeStackView.isHidden = false
-        self.homeStackView.alpha = 1
         
         let coreVersion: String = JIM.shared().getSDKVersion()
         let uikitVersion: String = JuggleUI.version
@@ -111,14 +77,6 @@ class ViewController: UIViewController {
         let type = ButtonType(rawValue: sender.tag)
         
         switch type {
-        case .user1:
-            JIM.shared().connectionManager.connect(withToken: token1)
-        case .user2:
-            JIM.shared().connectionManager.connect(withToken: token2)
-        case .user3:
-            JIM.shared().connectionManager.connect(withToken: token3)
-        case .user4:
-            JIM.shared().connectionManager.connect(withToken: token4)
         case .signIn:
             signinAction()
         default:
@@ -154,6 +112,11 @@ class ViewController: UIViewController {
                     return
                 }
                 JIM.shared().connectionManager.connect(withToken: token)
+            } else {
+                DispatchQueue.main.async {
+                    self.loadingIndicator.stopAnimating()
+                    self.view.isUserInteractionEnabled = true
+                }
             }
         }
     }
