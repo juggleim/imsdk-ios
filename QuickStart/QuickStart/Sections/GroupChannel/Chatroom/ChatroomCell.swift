24aaf38e14688b0a8cc8fea8d08fc94b936fcd08
Nathan <14392240@qq.com>
2024-09-05 18:46:49 +0800

    update chatroom cell

--- QuickStart/QuickStart/Sections/GroupChannel/Chatroom/ChatroomCell.swift
+++ QuickStart/QuickStart/Sections/GroupChannel/Chatroom/ChatroomCell.swift
@@ -10,10 +10,33 @@ import UIKit
 
 class ChatroomCell: SBUTableViewCell {
     // MARK: - UI properties
+    
+    public lazy var baseStackView: UIStackView = {
+        let stackView = SBUStackView(
+            axis: .horizontal,
+            alignment: .fill,
+            spacing: 16.0
+        )
+        stackView.distribution = .fill
+        return stackView
+    }()
+    
+    public lazy var portraitView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .center
+        imageView.clipsToBounds = true
+        imageView.isUserInteractionEnabled = true
+        return imageView
+    }()
+    
     public lazy var nameLabel = UILabel()
+    
+    public var separateView = UIView()
     
     @SBUThemeWrapper(theme: SBUTheme.userCellTheme)
     public var theme: SBUUserCellTheme
+    
+    internal private(set) var userImageSize: CGFloat = 32
     
     // MARK: - View Lifecycle
     override class func awakeFromNib() {
@@ -21,11 +44,41 @@ class ChatroomCell: SBUTableViewCell {
     }
     
     override func setupViews() {
-        self.contentView.addSubview(self.nameLabel)
+        self.baseStackView.setHStack([
+            self.portraitView,
+            self.nameLabel
+        ])
+        
+        self.contentView.addSubview(self.baseStackView)
+        self.contentView.addSubview(self.separateView)
     }
     
     override func setupLayouts() {
-        self.nameLabel.frame = CGRect(x: 16, y: 0, width: self.bounds.width - 32, height: self.bounds.height)
+        self.baseStackView
+            .sbu_constraint(
+                equalTo: self.contentView,
+                leading: 16,
+                trailing: -16,
+                top: 8,
+                bottom: 8
+            )
+            .sbu_constraint(height: userImageSize)
+
+        self.portraitView
+            .sbu_constraint(width: userImageSize, height: userImageSize)
+        
+        self.nameLabel
+            .setContentHuggingPriority(.required, for: .horizontal)
+        
+//        self.nameLabel.frame = CGRect(x: 16, y: 0, width: self.bounds.width - 32, height: self.bounds.height)
+        self.separateView
+            .sbu_constraint(equalTo: self.nameLabel, leading: 0)
+            .sbu_constraint(
+                equalTo: self.contentView,
+                trailing: -0.5,
+                bottom: 0.5
+            )
+            .sbu_constraint(height: 0.5)
     }
     
     override func updateLayouts() {
@@ -37,8 +90,22 @@ class ChatroomCell: SBUTableViewCell {
     override func setupStyles() {
         self.backgroundColor = theme.backgroundColor
         
+        self.portraitView.layer.cornerRadius = userImageSize/2
+        self.portraitView.backgroundColor = theme.userPlaceholderBackgroundColor
+        
+        self.portraitView.image = SBUIconSetType.iconSupergroup.image(
+            with: self.theme.userPlaceholderTintColor,
+            to: SBUIconSetType.Metric.defaultIconSize
+        )
+        self.portraitView.layer.cornerRadius = userImageSize/2
+        self.portraitView.backgroundColor = self.theme.mutedStateBackgroundColor
+        
         self.nameLabel.textColor = theme.nicknameTextColor
         self.nameLabel.font = theme.nicknameTextFont
+        
+        self.separateView.backgroundColor = theme.separateColor
+        
+        self.backgroundColor = theme.backgroundColor
     }
     
     // MARK: - Common
