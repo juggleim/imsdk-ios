9a434a895eb394a57a38187fb28e90839013f02e
Nathan <14392240@qq.com>
2024-10-09 15:22:16 +0800

    add jugglecall

---
+++ JuggleCall/JuggleCall/JuggleCall.h
@@ -0,0 +1,18 @@
+//
+//  JuggleCall.h
+//  JuggleCall
+//
+//  Created by Fei Li on 2024/10/9.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JuggleCall.
+FOUNDATION_EXPORT double JuggleCallVersionNumber;
+
+//! Project version string for JuggleCall.
+FOUNDATION_EXPORT const unsigned char JuggleCallVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JuggleCall/PublicHeader.h>
+
+
