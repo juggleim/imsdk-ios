b9d1b201752dccede75da1e8181bbc4e1dd77906
Nathan <14392240@qq.com>
2024-11-05 21:55:24 +0800

    remove useless code

--- JuggleIM/JuggleIM/call/internal/fsm/JConnectingState.m
+++ JuggleIM/JuggleIM/call/internal/fsm/JConnectingState.m
@@ -27,8 +27,6 @@
     switch (event) {
         case JCallEventJoinChannelDone:
             self.callSessionImpl.connectTime = [[NSDate date] timeIntervalSince1970] * 1000;
-            //TODO:
-            //[self.callSessionImpl signalChangeState];
             [self.callSessionImpl transitionToConnectedState];
             result = YES;
             break;
@@ -36,8 +34,6 @@
         case JCallEventJoinChannelFail:
             self.callSessionImpl.finishTime = [[NSDate date] timeIntervalSince1970] * 1000;
             self.callSessionImpl.finishReason = JCallFinishReasonNetworkError;
-            //TODO:
-            //[self.callSessionImpl signalChangeState];
             [self.callSessionImpl error:JCallErrorCodeJoinMediaRoomFail];
             [self.callSessionImpl transitionToIdleState];
             result = YES;
