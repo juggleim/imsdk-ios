7952aed2557e3b9eff7e7d45b2573314dd391cb0
Nathan <14392240@qq.com>
2025-08-20 17:13:20 +0800

    update 1.8.19

--- JuggleIM/JuggleIM/internal/JuggleIMConstInternal.h
+++ JuggleIM/JuggleIM/internal/JuggleIMConstInternal.h
@@ -7,7 +7,7 @@
 
 #define JPlatform @"iOS"
 #define JDeviceCompany @"Apple"
-#define JIMVersion @"1.8.18"
+#define JIMVersion @"1.8.19"
 #define jNaviAppKey @"x-appkey"
 #define jNaviToken @"x-token"
 
--- QuickStart/Sources/JuggleUI.swift
+++ QuickStart/Sources/JuggleUI.swift
@@ -9,6 +9,6 @@ import Foundation
 
 public class JuggleUI {
     
-    public static let version: String = "1.8.18"
+    public static let version: String = "1.8.19"
     
 }
