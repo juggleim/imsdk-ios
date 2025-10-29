bd70043b6cbc446a382bc79a8baa5292eb1ddda2
Nathan <14392240@qq.com>
2025-10-29 21:23:49 +0800

    update 1.8.25

--- JuggleIM/JuggleIM/internal/JuggleIMConstInternal.h
+++ JuggleIM/JuggleIM/internal/JuggleIMConstInternal.h
@@ -7,7 +7,7 @@
 
 #define JPlatform @"iOS"
 #define JDeviceCompany @"Apple"
-#define JIMVersion @"1.8.24.2"
+#define JIMVersion @"1.8.25"
 #define jNaviAppKey @"x-appkey"
 #define jNaviToken @"x-token"
 
