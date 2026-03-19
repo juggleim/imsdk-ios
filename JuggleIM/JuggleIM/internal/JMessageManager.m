8f8d0b8b6f5197a0f792ddf0ab9da145f10cf773
Nathan <14392240@qq.com>
2026-03-19 15:38:42 +0800

    add search message param

--- JuggleIM/JuggleIM/internal/JMessageManager.m
+++ JuggleIM/JuggleIM/internal/JMessageManager.m
@@ -574,14 +574,28 @@
     }
 }
 
-/// 搜素本地消息 SearchContent内容包含 指定内容
-/// - Parameter searchContent: 需要包含的字符串
 - (NSArray<JMessage *> *)searchMessagesWithContent:(NSString *)searchContent
                                     inConversation:(JConversation *)conversation
                                              count:(int)count
                                               time:(long long)time
                                          direction:(JPullDirection)direction
                                       contentTypes:(NSArray<NSString *> *)contentTypes {
+    return [self searchMessagesWithContent:searchContent
+                            inConversation:conversation
+                                     count:count
+                                      time:time
+                                 direction:direction
+                              contentTypes:contentTypes
+                             senderUserIds:nil];
+}
+
+- (NSArray<JMessage *> *)searchMessagesWithContent:(NSString *)searchContent
+                                    inConversation:(JConversation *)conversation
+                                             count:(int)count
+                                              time:(long long)time
+                                         direction:(JPullDirection)direction
+                                      contentTypes:(NSArray<NSString *> *)contentTypes
+                                     senderUserIds:(NSArray<NSString *> *)senderUserIds {
     if (!conversation || conversation.conversationId.length == 0) {
         return [NSArray array];
     }
@@ -594,7 +608,7 @@
                                                      time:time
                                             pullDirection:direction
                                              contentTypes:contentTypes
-                                                  senders:nil
+                                                  senders:senderUserIds
                                                    states:nil
                                             conversations:@[conversation]
                                         conversationTypes:nil
--- JuggleIM/JuggleIM/protocol/JMessageProtocol.h
+++ JuggleIM/JuggleIM/protocol/JMessageProtocol.h
@@ -472,6 +472,23 @@
                                          direction:(JPullDirection)direction
                                       contentTypes:(NSArray<NSString *> *)contentTypes;
 
+/// 消息本地检索
+/// - Parameters:
+///   - searchContent: 查询内容
+///   - conversation: 要查询的会话
+///   - count:拉取数量，超过 100 条按 100 返回
+///   - time: 消息时间戳，如果传 0 为当前时间
+///   - direction: 查询方向
+///   - contentTypes: 内容类型，传空返回所有类型
+///   - senderUserIds: 消息发送者 id 列表
+- (NSArray <JMessage *> *)searchMessagesWithContent:(NSString *)searchContent
+                                     inConversation:(JConversation *)conversation
+                                             count:(int)count
+                                              time:(long long)time
+                                         direction:(JPullDirection)direction
+                                      contentTypes:(NSArray<NSString *> *)contentTypes
+                                      senderUserIds:(NSArray <NSString *> *)senderUserIds;
+
 /// 根据消息中的关键字搜索会话
 /// - Parameters:
 ///   - option: 搜索条件
